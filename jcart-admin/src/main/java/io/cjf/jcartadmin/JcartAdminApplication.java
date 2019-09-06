87a9f655a9bfa4b011343025ea78ac3389f3b72f
chenjiefei <nlpx_dc@hotmail.com>
2019-09-06 21:48:08 +0800

    first init admin

---
+++ jcart-admin/src/main/java/io/cjf/jcartadmin/JcartAdminApplication.java
@@ -0,0 +1,13 @@
+package io.cjf.jcartadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JcartAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JcartAdminApplication.class, args);
+    }
+
+}
